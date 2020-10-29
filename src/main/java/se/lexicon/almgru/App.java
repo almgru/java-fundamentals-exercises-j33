b8f728338123ae07d5c68fafbd37e98019a25c1d
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2020-10-29 19:16:15 +0100

    Adds second exercise
    Creates 2 strings, one with first name and one with last name, and
    prints them out concatenated with a space.

--- src/main/java/se/lexicon/almgru/App.java
+++ src/main/java/se/lexicon/almgru/App.java
@@ -11,5 +11,11 @@ public class App {
         // 1. Create a String with your first name as content. Print out the String object to the console.
         String name = "Daniel";
         System.out.println(name);
+
+        /* 2. Create two Strings. One with your first name and the other with your last name. Print out both Strings to
+         * the console with a ” ” separating them. */
+        String firstName = "Daniel";
+        String lastName = "Alm Grundström";
+        System.out.println(firstName + " " + lastName);
     }
 }
