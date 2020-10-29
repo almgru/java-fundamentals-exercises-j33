08dbeba10b5d3ca829854db7562c83923393ff85
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2020-10-29 19:13:22 +0100

    Adds first exercise
    Adds a string called name and prints it out to the console.

--- src/main/java/se/lexicon/almgru/App.java
+++ src/main/java/se/lexicon/almgru/App.java
@@ -4,10 +4,12 @@ package se.lexicon.almgru;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+
+        // 1. Create a String with your first name as content. Print out the String object to the console.
+        String name = "Daniel";
+        System.out.println(name);
     }
 }
